ee43c687177ff0b39d18fe2180554e1d1fb90bcf
Aliia Satbakova <aliiasatbakova@MacBook-Air.local>
2024-06-10 12:40:28 +0700

    feat: добавлены модели

--- RickAndMorty/RickAndMorty/Model/Character.swift
+++ RickAndMorty/RickAndMorty/Model/Character.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+struct Character: Decodable {
+    let id: Int
+    let name: String
+    let status: CharacterStatus
+    let species: String
+    let type: String
+    let gender: Gender
+    let origin: Origin
+    let location: SingleLocation
+    let image: String
+    let episode: [String]
+    let url: String
+    let created: String
+}
+
+enum CharacterStatus: String, Decodable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case unknown = "unknown"
+}
+
+enum Gender: String, Decodable {
+    case male = "Male"
+    case female = "Female"
+    case genderless = "Genderless"
+    case unknown = "unknown"
+}
+
+struct Origin: Decodable {
+    let name: String
+    let url: String
+}
+
+struct SingleLocation: Codable {
+    let name: String
+    let url: String
+}
